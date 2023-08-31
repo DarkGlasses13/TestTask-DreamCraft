42e6b46e5b80fcdf1f5d606e691e36b72568a626
Matroskin_Cat <groznov3@gmail.com>
2023-08-31 11:49:17 +0300

    Local asset loader implementation

---
+++ Unity.TTDC/Assets/_Project/LocalAssetLoader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Assets._Project
+{
+    public class LocalAssetLoader : AssetLoader
+    {
+        public override async Task<T> LoadAndInstantiateAsync<T>(string key, Transform parent)
+        {
+            GameObject instance = await Addressables.InstantiateAsync(key, parent).Task;
+
+            if (instance.TryGetComponent(out T component))
+            {
+                Asset = instance;
+                return component;
+            }
+
+            throw new Exception(
+                $"Component of type {typeof(T)} could not be found on {instance.name}");
+        }
+
+        public override async Task<T> LoadAsync<T>(string key)
+        {
+            T asset = await Addressables.LoadAssetAsync<T>(key).Task;
+            Asset = asset;
+            return asset;
+        }
+
+        public override T LoadAndInstantiate<T>(string key, Transform parent)
+        {
+            AsyncOperationHandle<GameObject> loadingOperation = Addressables.InstantiateAsync(key, parent);
+            loadingOperation.WaitForCompletion();
+
+            if (loadingOperation.Result.TryGetComponent(out T component))
+            {
+                Asset = component;
+                return component;
+            }
+
+            throw new Exception(
+                $"Component of type {typeof(T)} could not be found on {loadingOperation.Result.name}");
+        }
+
+        public override T Load<T>(string key)
+        {
+            AsyncOperationHandle<T> loadingOperation = Addressables.LoadAssetAsync<T>(key);
+            loadingOperation.WaitForCompletion();
+            return loadingOperation.Result;
+        }
+
+        protected override void ReleaseAsset()
+        {
+            Addressables.Release(Asset);
+        }
+    }
+}
