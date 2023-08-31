5e9c46bd78bb8d886befd589859c1398292eb9e0
Matroskin_Cat <groznov3@gmail.com>
2023-08-31 17:02:08 +0300

    Character

---
+++ Unity.TTDC/Assets/_Project/Actors/Player Character/CharacterConfigLoader.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets._Project.Actors.Player_Character
+{
+    public class CharacterConfigLoader : LocalAssetLoader<CharacterConfig>
+    {
+        public override string Key => "Character Config";
+    }
+}
