43dfb12374cfabb997a3ac874135c4fac205b50f
Matroskin_Cat <groznov3@gmail.com>
2023-08-31 12:39:01 +0300

    Scene switching

---
+++ Unity.TTDC/Assets/_Project/Architecture/Scene Switching/ISceneSwitcher.cs
@@ -0,0 +1,7 @@
+namespace Assets._Project.Architecture.Scene_Switching
+{
+    public interface ISceneSwitcher
+    {
+        public void ChangeAsync(object key);
+    }
+}
---
+++ Unity.TTDC/Assets/_Project/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using Assets._Project.Architecture.Scene_Switching;
+using UnityEngine.AddressableAssets;
+
+namespace Assets._Project
+{
+    public class SceneSwitcher : ISceneSwitcher
+    {
+        public async void ChangeAsync(object key)
+        {
+            await Addressables.LoadSceneAsync("Empty").Task;
+            await Addressables.LoadSceneAsync(key).Task;
+        }
+    }
+}
