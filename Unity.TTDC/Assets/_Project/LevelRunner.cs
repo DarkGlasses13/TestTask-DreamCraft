81051db44aab46374dc8c8d7305e28686ff0f3dc
Matroskin_Cat <groznov3@gmail.com>
2023-08-31 11:51:21 +0300

    Project and level runners

---
+++ Unity.TTDC/Assets/_Project/LevelRunner.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets._Project
+{
+    public class LevelRunner : Runner
+    {
+        public LevelRunner(bool canEnableAllControllers) : base(canEnableAllControllers)
+        {
+            _controllers = new IController[]
+            {
+
+            };
+        }
+
+        protected override void OnControllersInitializedAndEnabled()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Unity.TTDC/Assets/_Project/ProjectRunner.cs
@@ -0,0 +1,39 @@
+﻿using Assets._Project.Architecture.DI;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets._Project
+{
+    public class ProjectRunner : Runner, IDIContainer
+    {
+        private readonly List<object> _cintainer = new();
+
+        public ProjectRunner(bool canEnableAllControllers) : base(canEnableAllControllers)
+        {
+            _controllers = new IController[]
+            {
+
+            };
+        }
+
+        public void Bind<T>(T dependency)
+        {
+            if (dependency != null && _cintainer.Contains(dependency) == false)
+            {
+                _cintainer.Add(dependency);
+            }
+        }
+
+        public T GetDependency<T>()
+        {
+            return (T)_cintainer.SingleOrDefault(service => service is T);
+        }
+
+        public void Unbind<T>(T dependency) => _cintainer.Remove(dependency);
+
+        protected override void OnControllersInitializedAndEnabled()
+        {
+
+        }
+    }
+}
